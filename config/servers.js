6d31fd56fae491396fdfc32e7cb89175dadf2939
ucupstudio <muhammadyusufbachtiar0@gmail.com>
2026-03-29 21:22:43 +0700

    Create servers.js

---
+++ config/servers.js
@@ -0,0 +1,12 @@
+const APP_CONFIG = {
+    MAIN_API_URL: "https://script.google.com/macros/s/AKfycbztTfrsbeJ_QveesN8Hov23WwHOr3Tee4FwIHtF-yrnlxxDSjFImTHdWQGXffMfpEdtzQ/exec",
+    SECRET_KEY: "3fb8f44a-71d5-4e1b-9a2c-9f6b3e8e7a10",
+    
+    SERVERS: {
+        "1": { name: "Server 1 (Default)", uploadUrl: "URL_GAS_AKUN_1_JIKA_ADA" },
+        "2": { 
+            name: "Server 2 (Media)", 
+            uploadUrl: "https://script.google.com/macros/s/AKfycbz82CdCRvIN0oFdkpUWhHpxSa7EUXKj_5SrXw2-wGTL6Inj7geMfd-0_ZUpy8g4H_mxSg/exec" 
+        }
+    }
+};
