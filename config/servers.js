fdd3600cdfc074339183511233511f9886eca5ba
ucupstudio <muhammadyusufbachtiar0@gmail.com>
2026-03-29 22:32:57 +0700

    Update servers.js

--- config/servers.js
+++ config/servers.js
@@ -1,12 +1,12 @@
 const APP_CONFIG = {
-    MAIN_API_URL: "https://script.google.com/macros/s/AKfycbztTfrsbeJ_QveesN8Hov23WwHOr3Tee4FwIHtF-yrnlxxDSjFImTHdWQGXffMfpEdtzQ/exec",
+    MAIN_API_URL: "https://script.google.com/macros/s/AKfycbwkXfahxyESa0nOsl5W_tINjSNRFqWxAoa4928TuPKd34AcSLAveIY8FujcpbW8L0D1Iw/exec",
     SECRET_KEY: "3fb8f44a-71d5-4e1b-9a2c-9f6b3e8e7a10",
     
     SERVERS: {
         "1": { name: "Server 1 (Default)", uploadUrl: "URL_GAS_AKUN_1_JIKA_ADA" },
         "2": { 
             name: "Server 2 (Media)", 
-            uploadUrl: "https://script.google.com/macros/s/AKfycbwQpacCRCKAnrUmNDan4R8PdMdEzo2mYZJsrMWdxcK5hd9SHzbRm-e57bQTN7Q-IXpybQ/exec" 
+            uploadUrl: "https://script.google.com/macros/s/AKfycbwU71_aPN7LdkTracqJqR3qLxZSECAIOH18fwGqoO27mjJ-bgWJwi9gJv2hjOsjvrIvGw/exec" 
         }
     }
 };
